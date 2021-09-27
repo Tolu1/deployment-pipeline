6525f4d04826a8e86e0642201948161f08bed628
Toluwani <tolugbesan@gmail.com>
2021-09-27 18:24:56 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import requests
 from time import sleep
 
 interval = 10
-url = 'https://47b4-105-112-180-98.ngrok.io/ping'
+url = 'https://a10a-105-112-72-191.ngrok.io/ping'
 
 # Ping dev server at intervals
 while True:
