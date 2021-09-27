3d330bd02aee6aec5d4cb92a946395cfed2b91a4
Toluwani <tolugbesan@gmail.com>
2021-09-27 19:24:13 +0100

    Update test.py

--- test.py
+++ test.py
@@ -7,6 +7,10 @@ url = 'https://a10a-105-112-72-191.ngrok.io/ping'
 
 # Ping dev server at intervals
 while True:
-    resp = requests.get(url)
-    print(resp.text)
-    sleep(interval)
+    try:
+        resp = requests.get(url,params={'message':'Hi Server!'})
+        print(resp.text)
+        sleep(interval)
+    except:
+        print('Restarting process after 10s')
+        sleep(10)
