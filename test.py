6a9f3401aed8268a653a8588f2dc409570f79450
Tolu <tolugbesan@gmail.com>
2021-09-27 22:33:20 +0100

    Updated test.py

--- test.py
+++ test.py
@@ -1,16 +1,27 @@
-# Self Test
+# Ahem...Testing, testing, 1, 2, 3
 import requests
 from time import sleep
 
-interval = 10
-url = 'https://a10a-105-112-72-191.ngrok.io/ping'
+# =========
+# Test Me!
+# =========
 
-# Ping dev server at intervals
+                                                            # ====================================
+                                                            # Change message and push code to see 
+                                                            # ping message updated on server end!
+                                                            # ====================================
+
+message = 'Hi there, Server!'                                                            
+INTERVAL = 10
+RESTART_AFTER = 10
+DEV_SERVER_HOSTNAME = 'https://a10a-105-112-72-191.ngrok.io' # remember to update this. 
+
+# Ping Development Server at intervals
 while True:
     try:
-        resp = requests.get(url,params={'message':'Hi there, Server!'})
+        resp = requests.get(url=f'{DEV_SERVER_HOSTNAME}/ping',params={'message':f'{message}'})
         print(resp.text)
-        sleep(interval)
+        sleep(INTERVAL)
     except:
-        print('Restarting process after 10s')
-        sleep(10)
+        print(f'Restarting process after {RESTART_AFTER}s')
+        sleep(RESTART_AFTER)
