2b0fee694b23010d4084b42b1092357ed370e879
Toluwani <tolugbesan@gmail.com>
2021-09-27 18:17:24 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print('This is a Test150')
+# Self Test
+import requests
+from time import sleep
+
+interval = 10
+url = 'https://47b4-105-112-180-98.ngrok.io/ping'
+
+# Ping dev server at intervals
+while True:
+    resp = requests.get(url)
+    print(resp.text)
+    sleep(interval)
