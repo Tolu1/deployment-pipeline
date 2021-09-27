84c505dab055a9fbec32f7d95f06c2a6763c2bc5
Toluwani <tolugbesan@gmail.com>
2021-09-27 19:28:06 +0100

    Update test.py

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ url = 'https://a10a-105-112-72-191.ngrok.io/ping'
 # Ping dev server at intervals
 while True:
     try:
-        resp = requests.get(url,params={'message':'Hi Server!'})
+        resp = requests.get(url,params={'message':'Hi there, Server!'})
         print(resp.text)
         sleep(interval)
     except:
